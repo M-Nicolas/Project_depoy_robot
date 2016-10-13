4305b1fa723bb2275d1e2a50f06b50d208088dd7
Meurgues Nicolas <meurgues.nicolas@gmail.com>
2016-10-13 11:41:56 +0400

    Functionning version working with going up

--- Assets/Scripts/GenerateSpheres.cs
+++ Assets/Scripts/GenerateSpheres.cs
@@ -3,19 +3,43 @@ using System.Collections;
 
 public class GenerateSpheres : MonoBehaviour {
 
-    public static bool generate;
+    public static bool generate = false;
     public GameObject robot;
+    public float waitTime;
+
+    private bool wait;
+    private float previousTime;
+
 
 	// Use this for initialization
 	void Start () {
-        if (generate)
-        {
-            Instantiate(robot);
-        }
-	}
+        Instantiate(robot);
+        wait = false;
+        previousTime = Time.time;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+        if (generate) {
+            Instantiate(robot);
+        }
+
+        GameObject[] list = GameObject.FindGameObjectsWithTag("Robots");
+
+        if (!wait) {
+            previousTime = Time.time;
+            for (int i = 0; i < list.Length; i++) {
+                Vector3 position = list[i].transform.position;
+                list[i].transform.position = new Vector3(position.x, position.y, position.z + 5);
+            }
+            wait = true;
+            generate = true;
+        } else {
+            if (previousTime < Time.time - waitTime) {
+                wait = false;
+            }
+            generate = false;
+        }
+    }
 }
