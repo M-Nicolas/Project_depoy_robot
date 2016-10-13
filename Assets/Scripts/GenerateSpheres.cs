7da03ee7e584f87dae6c4da776f2a74f3c19a922
Meurgues Nicolas <meurgues.nicolas@gmail.com>
2016-10-13 10:41:44 +0400

    Commit project base

---
+++ Assets/Scripts/GenerateSpheres.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateSpheres : MonoBehaviour {
+
+    public static bool generate;
+    public GameObject robot;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(robot);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
